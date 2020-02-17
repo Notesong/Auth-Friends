89e226665436cb503a09c95947e1de0f326dbc1c
Lee Damien <notesong.git@outlook.com>
2020-02-16 22:15:43 -0500

    Form set up

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Friends</h1>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </header>
+        <Switch>
 
-      </header>
-    </div>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>      
+    </Router>
   );
 }
 
