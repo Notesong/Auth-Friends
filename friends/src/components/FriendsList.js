869cbb020c090ef0a6ca440f5ee8dff72bf65ae9
Lee Damien <notesong.git@outlook.com>
2020-02-17 16:26:01 -0500

    Move friend form into own component

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -2,24 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Loader from "react-loader-spinner";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import Friend from './Friend';
+import FriendsListForm from './FriendsListForm';
 
 export default function FriendsList() {
   const [friendsList, setFriendsList] = useState([]);
-  const [name, setName] = useState('');
-  const [age, setAge] = useState('');
-  const [email, setEmail] = useState('');
-  const [isLoadingForm, showLoaderForm] = useState(false);
   const [isLoadingList, showLoaderList] = useState(false);
-  const [isAddingFriend, showAddingFriend] = useState(false);
-  const [formError, setFormError] = useState('');
   const [listError, setListError] = useState('');
-
-  const formReset = () => {
-    showLoaderForm(false);
-    setName('');
-    setAge('');
-    setEmail('');
-  }
 
   useEffect(() => {
     setListError('');
@@ -39,68 +27,11 @@ export default function FriendsList() {
     showLoaderList(false);
   }, []);
 
-  const onSubmit = e => {
-    e.preventDefault();
-    
-    setFormError('');
-    showLoaderForm(true);
-
-    axiosWithAuth()
-      .post("/friends", { name: name, age: parseInt(Math.round(age)), email: email })
-      .then(res => {
-        setFriendsList(res.data);
-        formReset();
-      })
-      .catch(err => {
-        setFormError('Error: Unable to add friend.');
-        formReset();
-      });
-  };
-
-  const showFriendForm = e => {
-    e.preventDefault()
-    showAddingFriend(!isAddingFriend)
-  }
-
   return (
     <div className="friends-list-component">
       <h2>Your Friends List</h2>
       <div className="friends-form-container">
-        {!isAddingFriend ? (
-          <button onClick={showFriendForm}>Add a Friend</button>
-        ) : (
-          <div className="form">
-            <button className="x-button" onClick={showFriendForm}>X</button>
-            <form className="form friend-form" onSubmit={onSubmit}>
-              {formError && <p className="error">{formError}</p>}
-              <p>Add a Friend</p>
-              <input
-                type="text"
-                placeholder="friend's name"
-                value={name}
-                required
-                onChange={e => setName(e.currentTarget.value)}
-              />
-              <input
-                type="text"
-                placeholder="friend's age"
-                value={age}
-                required
-                onChange={e => setAge(e.currentTarget.value)}
-              />
-              <input
-                type="email"
-                placeholder="friend's email"
-                value={email}
-                required
-                onChange={e => setEmail(e.currentTarget.value)}
-              />
-              <button className="submit" type="submit" disabled={isLoadingForm}>
-                {isLoadingForm ? 'Submitting...' : 'Submit'}
-              </button>
-            </form>
-          </div>
-        )}
+        <FriendsListForm setFriendsList={setFriendsList} />
       </div>
       <div className="friends-list">
         {listError && <p className="error">{listError}</p>}
@@ -111,7 +42,6 @@ export default function FriendsList() {
               color="#555555"
               height={30}
               width={100}
-              timeout={3000} //3 secs
             />
           ) : (
             <>
